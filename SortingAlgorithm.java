3ec6613227ae79d7363b935a6aeb928735770595
LucasZHsieh <l.hsieh.student@gmail.com>
2023-04-13 08:58:06 -0500

    cleaned up insertion sort and fixed bug

--- SortingAlgorithm.java
+++ SortingAlgorithm.java
@@ -1,38 +1,28 @@
-package laba_projects_homeworks;
+package laba_projects;
 
 import java.util.Arrays;
 
-/* Insertion sort, Selection sort, bubble sort, Merge sort, Quicksort. */
-
-public class SortingAlgorithm {
-	public static void main(String [] args) { 
-		
-		int[] arrayExample = {19, 1, 15 , 12 , 20};
-		int index = 0; 
-		
-		while (index != arrayExample.length -1) {
-			if (((index != 0) && (index != 1)) && (arrayExample[index-1] < arrayExample[index - 2])) { 
-				int store = arrayExample[index -1];
-				arrayExample[index - 1] = arrayExample[index-2]; 
-				arrayExample[index - 2] = store;
-				index -= 1;
-			}
-			else if (arrayExample[index] > arrayExample[index + 1]) {
-				int store = arrayExample[index];
-				arrayExample[index] = arrayExample[index+1]; 
-				arrayExample[index + 1] = store;
-				index += 1;
-			}
-			else { 
-				index += 1;
-			}
-
-			System.out.println(Arrays.toString(arrayExample));
-			
-		
-		}
-		System.out.println(Arrays.toString(arrayExample));
+public class InsertionAlgorithm {
+	public static void main(String[] args) {
+		int[] sortDemoArray = { 19, 202, 1, 24, 4, 10 };
+		System.out.println(Arrays.toString(InsertionSort(sortDemoArray)));
 	}
-	
-	
+
+	public static int[] InsertionSort(int[] arrayExample) {
+		int index = 0;
+		while (index != arrayExample.length - 1) {
+
+			if (arrayExample[index] > arrayExample[index + 1]) {
+				int store = arrayExample[index];
+				arrayExample[index] = arrayExample[index + 1];
+				arrayExample[index + 1] = store;
+				index = 0;
+			} else if (arrayExample[index] < arrayExample[index + 1]) {
+				index += 1;
+
+			}
+
+		}
+		return arrayExample;
+	}
 }
